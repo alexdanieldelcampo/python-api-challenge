1734b93a0f091e5898ae5f21c181119141e7e11c
Alexander del Campo <Alexander@macbook-pro.lan>
2021-01-26 16:06:45 -0800

    update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ea25883af2a293ca27013ac8be2bc3a6"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBkYYVNAkqtfU2gLmOUSBZywDuhXy4bsgY"
